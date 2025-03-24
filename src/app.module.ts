11073aa35ec86279da03a600e466bc6b92868de5
miketester10 <cadenza91@gmail.com>
2025-03-24 21:43:01 +0100

    add database migrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { GoogleAuthModule } from './auth/Google/google.module';
 import { JwtAuthModule } from './auth/JWT/jwt.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -8,6 +8,7 @@ import { UserModule } from './user/user.module';
 import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
 import { TodoModule } from './todo/todo.module';
+import { dataSourceOptions } from 'db/data-source';
 
 @Module({
   imports: [
@@ -31,20 +32,7 @@ import { TodoModule } from './todo/todo.module';
       isGlobal: true,
     }),
 
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: configService.get('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
-        entities: ['dist/**/*.entity{.ts,.js}'],
-        synchronize: configService.get('NODE_ENV') !== 'production',
-      }),
-    }),
+    TypeOrmModule.forRoot(dataSourceOptions),
 
     GoogleAuthModule,
     JwtAuthModule,
