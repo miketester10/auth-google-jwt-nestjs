d3f9b935c4a7c81743585eb4f026973f860680fd
miketester10 <cadenza91@gmail.com>
2025-03-28 20:41:53 +0100

    edit ErrrorResponseFilter to catch all errors, both HTTP and generic errors

--- src/common/filters/error-response.filter.ts
+++ src/common/filters/error-response.filter.ts
@@ -7,21 +7,32 @@ import {
   HttpException,
   HttpExceptionBody,
   HttpExceptionBodyMessage,
+  HttpStatus,
 } from '@nestjs/common';
 import { Response } from 'express';
 
-@Catch(HttpException)
+@Catch()
 export class ErrorResponseFilter implements ExceptionFilter {
-  catch(exception: HttpException, host: ArgumentsHost): void {
+  catch(exception: unknown, host: ArgumentsHost): void {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
-    const statusCode = exception.getStatus();
 
-    const responseException: string | object = exception.getResponse();
-    const message: HttpExceptionBodyMessage =
-      typeof responseException === 'string'
-        ? responseException
-        : (responseException as HttpExceptionBody).message;
+    // messaggio di default per tutti gli errori
+    let statusCode = HttpStatus.INTERNAL_SERVER_ERROR;
+    let message: HttpExceptionBodyMessage = 'Internal server error';
+
+    // messaggio personalizzato per gli errori HTTP
+    if (exception instanceof HttpException) {
+      statusCode = exception.getStatus();
+      const responseException: string | object = exception.getResponse();
+      message =
+        typeof responseException === 'string'
+          ? responseException
+          : (responseException as HttpExceptionBody).message;
+    } else if (exception instanceof Error) {
+      // messaggio personalizzato per gli errori di TypeScript
+      message = exception.message;
+    }
 
     response.status(statusCode).json({ message, statusCode });
   }
